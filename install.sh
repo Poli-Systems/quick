8822c07d6d9929e643806022a1dffeb37b88c65d
Poli <admin@poli.fun>
2020-04-16 11:30:41 +0200

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# Made by Poli
+echo "We do not recommand to run this script without knowing what it does. Starting now...."
---
+++ screen.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# Made by Poli
+if [ "$USER" = "root" ]; then
+    apt install screen -y
+    mkdir /home/temp
+    cd /home/temp
+    wget https://raw.githubusercontent.com/IIPoliII/Install-Script-For-New-Servers/master/Script/InstallAll.sh
+    chmod +x Install.sh
+    screen ./Install.sh
+    cd ..
+    rm -r /home/temp
+else
+    echo "Run this script as root or using sudo in the front of it !"
+fi
+service ssh restart
+echo "SSH service restarted you maybe need to reconnect"
+echo "Script Finished"
