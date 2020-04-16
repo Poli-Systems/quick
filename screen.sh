66f7edfeb895fbc1971293b69bfafda444e844d9
Poli <admin@poli.fun>
2020-04-16 11:34:45 +0200

    Update screen.sh

--- screen.sh
+++ screen.sh
@@ -4,7 +4,7 @@ if [ "$USER" = "root" ]; then
     apt install screen -y
     mkdir /home/temp
     cd /home/temp
-    wget https://raw.githubusercontent.com/IIPoliII/Install-Script-For-New-Servers/master/Script/InstallAll.sh
+    wget https://raw.githubusercontent.com/Poli-Systems/quick/master/install.sh
     chmod +x Install.sh
     screen ./Install.sh
     cd ..
