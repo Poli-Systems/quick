fb37b2151138c8f569a38429b4717d6d1b2336ae
Poli <admin@polisystems.ch>
2020-04-16 11:49:09 +0200

    Update

--- screen.sh
+++ screen.sh
@@ -5,13 +5,14 @@ if [ "$USER" = "root" ]; then
     mkdir /home/temp
     cd /home/temp
     wget https://raw.githubusercontent.com/Poli-Systems/quick/master/install.sh
-    chmod +x Install.sh
-    screen ./Install.sh
+    chmod +x InstallAll.sh
+    screen ./InstallAll.sh
     cd ..
     rm -r /home/temp
 else
     echo "Run this script as root or using sudo in the front of it !"
 fi
+rm -rf /home/temp
+echo "Script Finished"
 service ssh restart
 echo "SSH service restarted you maybe need to reconnect"
-echo "Script Finished"
