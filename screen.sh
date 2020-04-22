b05948639cdc1d4ae4248d17b4d2b408dd662c32
Poli <admin@poli.fun>
2020-04-22 12:39:15 +0200

    Update screen.sh

--- screen.sh
+++ screen.sh
@@ -12,6 +12,7 @@ else
     echo "Run this script as root or using sudo in the front of it !"
 fi
 rm -rf /home/temp
+apt update
 echo "Script Finished"
 service ssh restart
 echo "SSH service restarted you maybe need to reconnect"
