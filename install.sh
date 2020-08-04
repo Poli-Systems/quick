3a9297a94330928103f79c7a0a5378801ed0e533
Poli <raimundophilippe@gmail.com>
2020-08-04 10:46:34 +0200

    Added smartmontools

--- install.sh
+++ install.sh
@@ -18,7 +18,7 @@ echo "We do not recommand to run this script without knowing what it does. Strat
 	add-apt-repository universe
 
 #Installing a few packages
-	apt install -y openssh-server mysql-client wget curl nano zip unzip sed file libncurses5-dev libncursesw5-dev libssl-dev libpam0g-dev zlib1g-dev dh-autoreconf software-properties-common proftpd screen php htop build-essential make cmake scons make gcc g++ pkg-config curl
+	apt install -y openssh-server mysql-client smartmontools wget curl nano zip unzip sed file libncurses5-dev libncursesw5-dev libssl-dev libpam0g-dev zlib1g-dev dh-autoreconf software-properties-common proftpd screen php htop build-essential make cmake scons make gcc g++ pkg-config curl
 
 #Creating  an alias for ll instead of ls -l
         echo "alias ll='ls -l --color=auto'
