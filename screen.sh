80356e2951ac0d7ffeb86cf24499ee7278544688
Poli <raimundophilippe@gmail.com>
2020-08-25 17:42:10 +0200

    Update screen.sh

--- screen.sh
+++ screen.sh
@@ -16,4 +16,4 @@ rm -rf /home/temp
 apt update -y
 echo "Script Finished"
 service ssh restart
-echo "SSH service restarted you maybe need to reconnect"
+echo "SSH service restarted we recommand you to reconnect to avoid issues with backspace."
