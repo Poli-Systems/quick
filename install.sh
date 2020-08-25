c0ba64bb7d4c82117bc4645f543072b306ad8c1d
Poli <raimundophilippe@gmail.com>
2020-08-25 19:27:53 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -69,17 +69,60 @@ echo "We do not recommand to run this script without knowing what it does. Strat
 	fi
 	
 #Firehol with public ban lists (added in crontab as well)
-	echo "        server all accept" >> /etc/firehol/firehol.conf
-	update-ipsets enable feodo palevo sslbl zeus_badips dshield spamhaus_drop spamhaus_edrop openbl blocklist_de bi_ssh-ddos_0_1d bi_ssh-ddos_2_30d bi_ssh_0_1d bi_ssh_1_7d bi_ssh_2_30d bi_sshd_0_1d bi_wordpress_0_1d bi_wordpress_1_7d bi_wordpress_2_30d firehol_level1 firehol_level2 firehol_level3 dshield_top_1000 bruteforceblocker malc0de ransomware_rw greensnow myip fullbogons
+  echo '
+# FireHOL configuration file
+#
+# See firehol.conf(5) manual page and FireHOL Manual for details.
+#
+# This configuration file will allow all requests originating from the
+# local machine to be send through all network interfaces.
+#
+# No requests are allowed to come from the network. The host will be
+# completely stealthed! It will not respond to anything, and it will
+# not be pingable, although it will be able to originate anything
+# (even pings to other hosts).
+#
+
+version 6
+
+        ipset4 create whitelist hash:net
+        ipset4 add whitelist 127.0.0.1/32
+
+        # subnets - netsets
+        for x in fullbogons dshield spamhaus_drop spamhaus_edrop firehol_level1 firehol_level2 firehol_level3 dshield_1d dshield_30d dshield_7d
+        do
+                ipset4 create  ${x} hash:net
+                ipset4 addfile ${x} ipsets/${x}.netset
+                blacklist4 full inface any log "BLACKLIST ${x^^}" ipset:${x} \
+                        except src ipset:whitelist
+        done
+
+        # individual IPs - ipsets
+        for x in blocklist_de bruteforceblocker malc0de myip greensnow dshield_top_1000
+        do
+                ipset4 create  ${x} hash:ip
+                ipset4 addfile ${x} ipsets/${x}.ipset
+                blacklist4 full inface any log "BLACKLIST ${x^^}" ipset:${x} \
+                        except src ipset:whitelist
+        done
+
+
+# Accept all client traffic on any interface
+interface any world
+        client all accept
+        server all accept
+' > /etc/firehol/firehol.conf
+
+	update-ipsets enable  dshield spamhaus_drop spamhaus_edrop blocklist_de firehol_level1 firehol_level2 firehol_level3 dshield_top_1000 bruteforceblocker malc0de greensnow myip fullbogons
 	update-ipsets -s
 	
 	crontab -l > /tmp/tmpcron
 	echo "*/13 * * * * root update-ipsets -s >/dev/null 2>&1" >> /tmp/tmpcron
 	crontab /tmp/tmpcron
 	rm /tmp/tmpcron
-	
+  	
 	update-rc.d firehol defaults
-	firehol start
+	firehol restart
 	
 #Select the best mirror on ubuntu
 #	apt install 'python(3?)-bs4$' -y
